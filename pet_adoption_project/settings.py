b7de9aa302e8bdf22941055f3d7e582f9e5b2f98
Saskia <saskia.tjioe@gmail.com>
2023-11-09 16:09:49 -0500

    Set up PostgreSQL connection. No migrations.

--- pet_adoption_project/settings.py
+++ pet_adoption_project/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'pet_adoption_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
     }
 }
 
